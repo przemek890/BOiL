b3a285d87fb0c51d361bb2639c63510a00afd947
przemek890 <przemek.899@wp.pl>
2024-02-26 23:03:10 +0100

    -

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from src.CreateDataframe import create_activities, create_events
+from src.Create import create_activities, create_events, create_table
 from src.PlotGraph import Gplot
 ''''''''''''''''''
 activities = create_activities()
-print(activities)
+print("Activities: ",activities,end="\n\n")
 
 events = create_events()
-print(events)
+print("Events: ",events,end="\n\n")
+
+table = create_table(activities,events)
+print(table)
 
 Gplot(activities,events)
 

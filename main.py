03a4f455e900236c44f4f834b3ac45e77715479f
przemek890 <przemek.899@wp.pl>
2024-02-26 23:48:03 +0100

    -

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.Create import create_activities, create_events, create_table
 from src.PlotGraph import Gplot
+from tabulate import tabulate
 ''''''''''''''''''
 activities = create_activities()
 print("Activities: ",activities,end="\n\n")
@@ -7,9 +8,9 @@ print("Activities: ",activities,end="\n\n")
 events = create_events()
 print("Events: ",events,end="\n\n")
 
-table = create_table(activities,events)
-print(table)
+table = create_table(activities)
+print(tabulate(table, headers='keys', tablefmt='simple', numalign='center', stralign='center'))
 
-Gplot(activities,events)
+Gplot(activities,events,table)
 
 

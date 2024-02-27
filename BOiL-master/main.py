e869d5e1916429fd467f990c8c7c6f9c6add9c0f
andrz <ajanosz@student.pl>
2024-02-27 16:05:41 +0100

    pierwszy commit Janosz

---
+++ BOiL-master/main.py
@@ -0,0 +1,21 @@
+from src.logic.Create import create_activities, create_events, create_table
+from src.logic.PlotGraph import Gplot
+from tabulate import tabulate
+from src.view.Window import start_note
+''''''''''''''''''
+activities = create_activities()
+print("Activities: ",activities,end="\n\n")
+
+events = create_events()
+print("Events: ",events,end="\n\n")
+
+table = create_table(activities)
+print(tabulate(table, headers='keys', tablefmt='simple', numalign='center', stralign='center'))
+
+#Gplot(activities,events,table)
+
+#jeszcze nie działa
+# Gantt(activities)
+
+# UI - 1.0:
+start_note()

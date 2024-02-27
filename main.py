feb6d8acefa143ffac288f948bad86a2d1a9436a
przemek890 <przemek.899@wp.pl>
2024-02-27 12:15:48 +0100

    -

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from src.Create import create_activities, create_events, create_table
-from src.PlotGraph import Gplot
+from src.logic.Create import create_activities, create_events, create_table
+from src.logic.PlotGraph import Gplot
+from src.view.Window import start_note
 from tabulate import tabulate
 ''''''''''''''''''
+# Czynności poprzedzające:
 activities = create_activities()
 print("Activities: ",activities,end="\n\n")
 
+# Numeracja zdarzeń:
 events = create_events()
 print("Events: ",events,end="\n\n")
 
+# Tablica metody CPM:
 table = create_table(activities)
 print(tabulate(table, headers='keys', tablefmt='simple', numalign='center', stralign='center'))
 
+# Poglądowy graf - pdf:
 Gplot(activities,events,table)
 
-
+# UI - 1.0:
+start_note()

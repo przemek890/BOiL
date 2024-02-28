ed3304eacf0475d2e80c867d2c893948bda85702
wiktor <wiktor.k.2002@gmail.com>
2024-02-29 00:22:04 +0100

    react+electron

--- src/view/my-new-app/src/app.js
+++ src/view/my-new-app/src/app.js
@@ -2,4 +2,6 @@ import * as React from "react";
 import {render} from "react-dom";
 import Main_window from "./main_window.jsx";
 
+
+
 render(<Main_window></Main_window>, document.getElementById("root"))

12c6185c13f80eba82096a6880f88d26c306d24b
wiktor <wiktor.k.2002@gmail.com>
2024-02-29 17:53:35 +0100

    -

--- src/logic/Gantt.py
+++ src/logic/Gantt.py
@@ -5,6 +5,40 @@ import pandas as pd
 import plotly.express as ex
 from src.logic.Create import create_table
 
+def draw_arrow_between_jobs(fig, first_job_dict, second_job_dict):
+    ## retrieve tick text and tick vals
+    job_yaxis_mapping = dict(zip(fig.layout.yaxis.ticktext,fig.layout.yaxis.tickvals))
+    jobs_delta = second_job_dict['Start'] - first_job_dict['Finish']
+    ## horizontal line segment
+    fig.add_shape(
+        x0=first_job_dict['Finish'], y0=job_yaxis_mapping[first_job_dict['Task']],
+        x1=first_job_dict['Finish'] + jobs_delta/2, y1=job_yaxis_mapping[first_job_dict['Task']],
+        line=dict(color="blue", width=2)
+    )
+    ## vertical line segment
+    fig.add_shape(
+        x0=first_job_dict['Finish'] + jobs_delta/2, y0=job_yaxis_mapping[first_job_dict['Task']],
+        x1=first_job_dict['Finish'] + jobs_delta/2, y1=job_yaxis_mapping[second_job_dict['Task']],
+        line=dict(color="blue", width=2)
+    )
+    ## horizontal line segment
+    fig.add_shape(
+        x0=first_job_dict['Finish'] + jobs_delta/2, y0=job_yaxis_mapping[second_job_dict['Task']],
+        x1=second_job_dict['Start'], y1=job_yaxis_mapping[second_job_dict['Task']],
+        line=dict(color="blue", width=2)
+    )
+    ## draw an arrow
+    fig.add_annotation(
+        x=second_job_dict['Start'], y=job_yaxis_mapping[second_job_dict['Task']],
+        xref="x",yref="y",
+        showarrow=True,
+        ax=-10,
+        ay=0,
+        arrowwidth=2,
+        arrowcolor="blue",
+        arrowhead=2,
+    )
+    return fig
 
 def formatuj_czas_na_date(czas):
     delta_czasu = timedelta(hours=czas)
@@ -30,7 +64,7 @@ class Zadanie:
 
 def Gantt(activities):
     table = create_table(activities)
-    tw = pd.DataFrame({"Task":table["Czynność"],"Start":[formatuj_czas_na_date(i) for i in table["LS"]],"Finish":[formatuj_czas_na_date(i) for i in table["LF"]]})
+    tw = pd.DataFrame({"Task":table["Czynność"],"Start":[formatuj_czas_na_date(i) for i in table["ES"]],"Finish":[formatuj_czas_na_date(i) for i in table["EF"]]})
 
     fig = ex.timeline(tw, x_start="Start", x_end="Finish", y="Task", color="Task")
     fig.update_yaxes(autorange="reversed")

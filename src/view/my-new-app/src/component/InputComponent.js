431c053e7977d06258e32976c8a0d40394f4fec3
przemek890 <przemek.899@wp.pl>
2024-03-03 22:45:37 +0100

    -

--- src/view/my-new-app/src/component/InputComponent.js
+++ src/view/my-new-app/src/component/InputComponent.js
@@ -2,59 +2,41 @@ import React, { useEffect, useState } from 'react';
 import "./Input/style.css"
 
 const InputComponent = () => {
-  const [htmlData, setHtmlData] = useState(null
-  );
+  const [htmlData, setHtmlData] = useState(null);
   const [cssData, setCssData] = useState(null);
   const [jsData, setJsData] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:5000/get_html')
-
-      .then(response => response.blob())
-      .then(data => {
-        const file = URL.createObjectURL(data);
-        setHtmlData(file);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-    fetch('http://localhost:5000/get_css')
-      .then(response => response.text())
-      .then(data => {
-        setCssData(data);
-      })
-
-      .catch((error) => {
-        console.error('Error:', error);
-
-      });
-    fetch('http://localhost:5000/get_js')
-      .then(response => response.text())
-      .then(data => {
-        setJsData(data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
+    Promise.all([
+      fetch('http://localhost:5000/get_html').then(response => response.blob()),
+      fetch('http://localhost:5000/get_css').then(response => response.text()),
+      fetch('http://localhost:5000/get_js').then(response => response.text())
+    ])
+    .then(([htmlData, cssData, jsData]) => {
+      setHtmlData(URL.createObjectURL(htmlData));
+      setCssData(cssData);
+      setJsData(jsData);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   }, []);
-
 
   useEffect(() => {
     if (htmlData && cssData && jsData) {
       const iframe = document.querySelector('iframe');
-
       iframe.onload = function() {
         const doc = iframe.contentWindow.document;
         if (doc.body) {
           const styleElement = doc.createElement('style');
-
           styleElement.textContent = cssData;
-
           doc.head.appendChild(styleElement);
 
-          const scriptElement = doc.createElement('script');
-          scriptElement.textContent = jsData;
-          doc.body.appendChild(scriptElement);
+          setTimeout(() => {
+            const scriptElement = doc.createElement('script');
+            scriptElement.textContent = jsData;
+            doc.body.appendChild(scriptElement);
+          }, 1000);
         }
       };
       iframe.src = htmlData;

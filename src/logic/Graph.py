71d043098bf2b0f858ce4c2eac5949999b5419ac
Julia <julia.kluk6@wp.pl>
2024-03-07 20:47:04 +0100

    :)

--- src/logic/Graph.py
+++ src/logic/Graph.py
@@ -1,10 +1,36 @@
 import networkx as nx
 import plotly.graph_objects as go
+from src.logic.Create import create_table
+import pandas as pd
 
 def Graph(activities):
+    #wczytywanie danych
+    ky =[]
+    for a, b in activities.items():
+        ky.append(a)
+
+    val = [x for x in activities.values()]
+    cp = []
+    for i in val:
+        pom = []
+        for j in i:
+            pom.append(j[0])
+        cp.append(pom)
+
+    da = pd.DataFrame({"Task": ky,"PreTask": cp})
+    print(da)
+
+
     G = nx.Graph()
-    G.add_edge(1, 2)
-    G.add_edge(2, 3)
+    G.add_edge(1, 2) #A
+    G.add_edge(1, 3) #B
+    G.add_edge(2, 4) #C
+    G.add_edge(2, 5) #D
+    G.add_edge(3, 5) #E
+    G.add_edge(4, 5) #F
+    G.add_edge(4, 6) #G
+    G.add_edge(5, 6) #H
+
     #
     pos = nx.spring_layout(G)
 
@@ -14,13 +40,10 @@ def Graph(activities):
     for edge in G.edges():
         x0, y0 = pos[edge[0]]
         x1, y1 = pos[edge[1]]
-        edge_x.append(x0)
-        edge_x.append(x1)
-        edge_x.append(None)
-        edge_y.append(y0)
-        edge_y.append(y1)
-        edge_y.append(None)
+        edge_x += [x0, x1, None]
+        edge_y += [y0, y1, None]
 
+    #
     edge_trace = go.Scatter(
         x=edge_x, y=edge_y,
         line=dict(width=0.5, color='#888'),
@@ -35,22 +58,21 @@ def Graph(activities):
         node_x.append(x)
         node_y.append(y)
 
+    node_labels = [str(node) for node in G.nodes()]
+
     node_trace = go.Scatter(
         x=node_x, y=node_y,
-        mode='markers',
+        mode='markers+text',  # Dodanie etykiet
+        text=node_labels,  # Tekst etykiet
+        textposition='middle center',  # Pozycja tekstu względem węzłów
         hoverinfo='text',
         marker=dict(
-            showscale=True,
+            showscale=False,
             colorscale='YlGnBu',
-            size=10,
-            colorbar=dict(
-                thickness=15,
-                title='Node Connections',
-                xanchor='left',
-                titleside='right'
-            )
+            size=40,
         )
     )
+
 
     fig = go.Figure(data=[edge_trace, node_trace],
                     layout=go.Layout(
@@ -61,6 +83,26 @@ def Graph(activities):
                         yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
                     )
 
+    # pom = []
+    # pom2=[]
+    # for indeks, wiersz in da.iterrows():
+    #     pom.append(indeks+1)
+    #     # print(wiersz[1])
+    #     if(wiersz[1][0]!='-'):
+    #         pom2.append(ord(wiersz[1][0])-64)
+    #     else
+    #
+    #
+    # print(pom)
+    # print(pom2)
+
+    # fig.add_trace(go.Scatter(x=[formatuj_czas_na_date(row["ES"])],
+    #                                          y=[row["Czynność"]],
+    #                                          mode="markers",
+    #                                          marker=dict(color="red", size=10),
+    #                                          showlegend=False,
+    #                                          hoverinfo='text',
+    #                                          text=', '.join(dependencies) + f' -> {row["Czynność"]}'))
 
     return fig.to_json()
     # G = nx.Graph()
